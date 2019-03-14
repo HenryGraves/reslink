4826073a5def50f9cc979ed37bdeb4e2f95007c7
Henry <henrygraves95@gmail.com>
2019-03-14 12:10:52 -0700

    initial server code and blank front-end

---
+++ server/server.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const path = require('path');
+const app = express();
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname + '/../app/index.html'));
+});
+app.get('/hi', (req,res) => {
+    res.send("Hello")
+});
+const PORT = process.env.PORT || 8080;
+app.listen(PORT, () => {
+    console.log('Server listening on port ${PORT}');
+});
