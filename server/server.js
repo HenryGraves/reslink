1d5f76357bd6949a56ac3d23bbf5a0d599e92f39
Henry <henrygraves95@gmail.com>
2019-03-19 13:41:42 -0700

    added more info to be console.logged() when initializing

--- server/server.js
+++ server/server.js
@@ -14,5 +14,6 @@ app.get("*", (req, res) => {
 });
 const PORT = process.env.PORT || 8080;
 app.listen(PORT, () => {
-    console.log('Server listening on port ${PORT}');
+    console.log('Server listening on port', PORT);
+    console.log('Visit: https://localhost:8080/ to browse app');
 });
